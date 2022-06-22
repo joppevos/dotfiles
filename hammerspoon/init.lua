664d321b07e6c0aa27e8d3cc9b1db2f68ee59666
joppevos <joppe.vos@gmail.com>
2022-06-22 10:32:28 +0200

    Lua update

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -10,11 +10,13 @@ function open_app(name)
     end
 end
 
---- quick open applications
-hs.hotkey.bind({"command", "shift"}, "1", open_app("iTerm"))
-hs.hotkey.bind({"command", "shift"}, "2", open_app("Google Chrome"))
-hs.hotkey.bind({"command", "shift"}, "i", open_app("Microsoft Teams"))
-hs.hotkey.bind({"command", "shift"}, "p", open_app("Pycharm"))
 
-hs.hotkey.bind({"command", "shift"}, "7", open_app("Calendar"))
-hs.hotkey.bind({"command", "shift"}, "8", open_app("Mail"))
+--- quick open applications
+--
+hs.hotkey.bind({"option", "space"}, "9", open_app("iTerm"))
+hs.hotkey.bind({"option", "space"}, "8", open_app("Google Chrome"))
+hs.hotkey.bind({"option", "space"}, "t", open_app("Microsoft Teams"))
+hs.hotkey.bind({"option", "space"}, "p", open_app("Pycharm"))
+
+hs.hotkey.bind({"option", "space"}, "n", open_app("Calendar"))
+hs.hotkey.bind({"option", "space"}, "l", open_app("Mail"))
